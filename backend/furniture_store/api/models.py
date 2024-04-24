c0ce0f6dd2529ff7358d7122d41f208a97ce24c1
Oryngali Dauir <123401433+DauirEZ@users.noreply.github.com>
2024-04-25 00:45:07 +0600

    Update models.py

--- backend/furniture_store/api/models.py
+++ backend/furniture_store/api/models.py
@@ -5,13 +5,6 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
-    # user = models.ForeignKey(
-    #     User,
-    #     on_delete=models.CASCADE,
-    #     related_name='categories',
-    #     null=True, blank=True
-    # )
-
     def __str__(self):
         return self.name
 
